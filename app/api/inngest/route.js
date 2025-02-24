f71bfd0b7bb79ea09c6d36c28846dfa427224cb3
Udayan-Mal <udayan.mal03@gmail.com>
2025-02-25 04:20:09 +0530

    Fixed import for connectDB

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,13 +1,34 @@
-import { serve } from "inngest/next";
-import { inngest, syncUserCreation, syncUserDeletion, syncUserUpdation } from "@/config/inngest";
+// import { serve } from "inngest/next";
+// import { inngest, syncUserCreation, syncUserDeletion, syncUserUpdation } from "@/config/inngest";
 
-// Create an API that serves zero functions
+// // Create an API that serves zero functions
+// export const { GET, POST, PUT } = serve({
+//   client: inngest,
+//   functions: [
+//     syncUserCreation,
+//     syncUserDeletion,
+//     syncUserUpdation,
+//     createUserOrder
+//   ],
+// });
+
+
+import { serve } from 'inngest/next';
+import {
+  inngest,
+  syncUserCreation,
+  syncUserDeletion,
+  syncUserUpdation,
+  createUserOrder, // Add this import
+} from '@/config/inngest';
+
+// Create an API that serves multiple functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
     syncUserCreation,
     syncUserDeletion,
     syncUserUpdation,
-    createUserOrder
+    createUserOrder, // Add this function
   ],
 });
